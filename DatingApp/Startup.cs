152fa3362d59496e944806389150b1a8b2520407
Nhlahla Mike <nhlahlamikemabasa@gmail.com>
2020-01-23 09:17:48 +0200

    End of Section 13

--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -57,6 +57,7 @@ namespace DatingApp
 			services.AddTransient<Seed>();
 			services.AddScoped<IAuthRepository, AuthRepository>();
 			services.AddScoped<IDatingRepository, DatingRepository>();
+			services.AddScoped<LogUserActivity>();
 			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 				.AddJwtBearer(options =>
 				{
